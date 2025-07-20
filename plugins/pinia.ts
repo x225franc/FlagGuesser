5c9ecf35df925fd7d2d1980a85e33d6c1b6b272c
Alpha Malick DIAWARA <102668503+x225franc@users.noreply.github.com>
2025-07-20 21:10:24 +0200

    Refactor Pinia plugin to unify client and server logic

--- plugins/pinia.ts
+++ plugins/pinia.ts
@@ -3,5 +3,10 @@ import { createPinia } from "pinia";
 
 export default defineNuxtPlugin((nuxtApp) => {
 	const pinia = createPinia();
+	if (process.client) {
+		const piniaPluginPersistedstate =
+			require("pinia-plugin-persistedstate").default;
+		pinia.use(piniaPluginPersistedstate);
+	}
 	nuxtApp.vueApp.use(pinia);
 });
